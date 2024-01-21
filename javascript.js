99a138d8ae34aace07355b04055aea77d60c4f35
ErselEngin <70911603+ErselEngin@users.noreply.github.com>
2024-01-21 12:24:25 +0300

    Long Awaited UI Update!
    Now playable with a UI instead of console
    Basic functionality

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
 let player = "";
 let computer = "";
-console.log(game());
+let playerBtn = document.querySelectorAll('.playerChoice');
+let result = document.querySelector('.results');
+//console.log(game());
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
+    const compRock = document.querySelector('#compRock');
+    const compPaper = document.querySelector('#compPaper');
+    const compScissors = document.querySelector('#compScissors');
+    
+    compRock.setAttribute('style', "");
+    compPaper.setAttribute('style', "");
+    compScissors.setAttribute('style', "");
+
+    if(computerChoice === 1)
+    {
+        
+        compRock.setAttribute('style', "border: 2px green solid;")
+    }
+    if(computerChoice === 2)
+    {
+        
+        compPaper.setAttribute('style', "border: 2px green solid;")
+    }
+    if(computerChoice === 3)
+    {
+        
+        compScissors.setAttribute('style', "border: 2px green solid;")
+    }
 
     switch (computerChoice) {
         case 1: return "Rock"
@@ -12,14 +37,14 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock? Paper? Scissors?")
-    return playerChoice;
-}
+// function getPlayerChoice() {
+//     let playerChoice = prompt("Rock? Paper? Scissors?")
+//     return playerChoice;
+// }
 
-function playRound() {
+function playRound(played) {
 
-    player = getPlayerChoice();
+    player = played;
     computer = getComputerChoice();
 
     if (player.toLowerCase() === "rock" && computer === "Scissors") {
@@ -46,45 +71,66 @@ function playRound() {
 }
 
 
-function game() {
+// function game() {
 
-    let counter = 0;
-    let playerScore = 0;
-    let computerScore = 0;
+//     let counter = 0;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    while (counter < 5) {
-        let round = playRound(player, computer);
+//     while (counter < 5) {
+//         let round = playRound(player, computer);
 
-        if (round === 1) {
-            console.log("You Win! " + player + " beats " + computer);
-            ++playerScore;
-            ++counter;
+//         if (round === 1) {
+//             console.log("You Win! " + player + " beats " + computer);
+//             ++playerScore;
+//             ++counter;
+//         }
+//         else if (round === 2) {
+//             console.log("You Lose!" + computer + " beats " + player);
+//             ++computerScore;
+//             ++counter;
+//         }
+//         else {
+//             console.log("It's a Tie!")
+//             ++counter;
+//         }
+//     }
+
+//     if (playerScore > computerScore) {
+//         console.log("Player Wins The Game!")
+
+//         if (confirm("New Game?")) {
+//             console.clear();
+//             game();
+//         }
+//     }
+//     else {
+//         console.log("Computer Wins The Game!")
+
+//         if (confirm("New Game?")) {
+//             console.clear();
+//             game();
+//         }
+//     }
+// }
+
+playerBtn.forEach((button => {
+    button.addEventListener('click', () =>{
+        button.setAttribute('style', "border: 2px green solid");
+
+        let r = playRound(button.textContent);
+        if(r){
+            result.textContent = "Player Wins";
         }
-        else if (round === 2) {
-            console.log("You Lose!" + computer + " beats " + player);
-            ++computerScore;
-            ++counter;
+        if (r === 2){
+            result.textContent = "Computer Wins";
         }
-        else {
-            console.log("It's a Tie!")
-            ++counter;
+        if(r === 0){
+            result.textContent = "It's a Tie!"
         }
-    }
 
-    if (playerScore > computerScore) {
-        console.log("Player Wins The Game!")
-
-        if (confirm("New Game?")) {
-            console.clear();
-            game();
-        }
-    }
-    else {
-        console.log("Computer Wins The Game!")
-
-        if (confirm("New Game?")) {
-            console.clear();
-            game();
-        }
-    }
-}
+        setTimeout(() => {
+            button.setAttribute('style', "");
+        }, 2000);
+    })
+}))
